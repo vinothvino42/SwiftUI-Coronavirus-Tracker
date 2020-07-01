41a3b98e032edf81e511ef51365d4f40e00e90a6
Vinoth Vino <vinothvino42@gmail.com>
2020-07-01 23:16:30 +0530

    Initial Commit

---
+++ Coronavirus Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coronavirus Tracker
+//
+//  Created by Vinoth Vino on 01/07/20.
+//  Copyright © 2020 Vinoth Vino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
